9083becce853e93b93e3af549b187cec36904b7c
Agustin-Gonzalorena <agonzalorena1@gmail.com>
2024-05-31 13:54:49 -0300

    update footer

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <BgAnimated />
-      <main className="w-full flex justify-center">
+      <main className="w-full min-h-[80vh] flex justify-center">
         <div className="w-full mb-10 md:max-w-2xl lg:max-w-4xl mt-10 min-h-60 bg-[#242424e0] p-5 shadow-black shadow-lg">
           <Title />
           <TablePrice />
@@ -28,7 +28,7 @@ function App() {
           </section>
         </div>
       </main>
-      <footer className="w-full bg-black min-h-32 text-white flex flex-col justify-center items-center gap-2">
+      <footer className="w-full bg-black min-h-[20vh] text-white flex flex-col justify-center items-center gap-2">
         <p className="text-sm font-thin">Copyright © {year}</p>
         <p>
           Creado por{" "}
